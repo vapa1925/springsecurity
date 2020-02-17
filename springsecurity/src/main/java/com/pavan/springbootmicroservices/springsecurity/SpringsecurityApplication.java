2427d0e03bbafd8d99fb88a4396ed72205579f95
pavan <pavan.manne1925@gmail.com>
2020-02-17 18:22:24 +0530

    Initial commit

---
+++ springsecurity/src/main/java/com/pavan/springbootmicroservices/springsecurity/SpringsecurityApplication.java
@@ -0,0 +1,13 @@
+package com.pavan.springbootmicroservices.springsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringsecurityApplication.class, args);
+	}
+
+}
